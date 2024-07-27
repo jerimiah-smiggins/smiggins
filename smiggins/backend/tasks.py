27ce7044ff3edbf2a553b69d2d7379354ea7f97d
trinkey <97406176+trinkey@users.noreply.github.com>
2024-07-27 16:31:14 -0400

    task actually works

--- smiggins/backend/tasks.py
+++ smiggins/backend/tasks.py
@@ -4,13 +4,11 @@ import time
 from posts.models import URLPart
 
 def remove_extra_urlparts():
-    print("r")
     current_time = round(time.time())
 
     for i in URLPart.objects.all():
         if i.expire <= current_time:
-            print(i.url)
             i.delete()
 
     # Rerun after two hours
-    threading.Timer(60 * 2, remove_extra_urlparts)
+    threading.Timer(60 * 60 * 2, remove_extra_urlparts).start()
