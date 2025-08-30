6a8fb043f81962ceb4a489a8ac6cb41059c7a562
trinkey <trinkey@duck.com>
2025-08-30 08:50:17 -0400

    add logs to compile script

--- compile.sh
+++ compile.sh
@@ -1,14 +1,23 @@
 if [ "$1" != "less" ]; then
+  printf "compiling typescript..."
   tsc
+
+  if [ "$1" != "uncompressed" ]; then
+    printf " compressing..."
+    uglifyjs ./smiggins/static/app.js --output ./smiggins/static/app.js
+  fi
+
+  echo " done"
 fi
 
 if [ "$1" != "ts" ]; then
+  printf "compiling less..."
   lessc ./smiggins/less/base.less ./smiggins/static/base.css
-fi
 
-cd ./smiggins/static;
+  if [ "$1" != "uncompressed" ]; then
+    printf " compressing..."
+    uglifycss ./smiggins/static/base.css --output ./smiggins/static/base.css
+  fi
 
-if [ "$1" != "uncompressed" ]; then
-  uglifycss base.css --output base.css
-  uglifyjs app.js --output app.js
+  echo " done"
 fi
